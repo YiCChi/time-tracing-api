59c19fc0dad3394de6dbccb1864d418f1b01b50b
cyan0515 <ballpen517@yahoo.co.jp>
2022-11-13 11:53:00 +0900

    Revert "feat: ログイン機構の作成"
    This reverts commit 86c56ab8775f561583e604e8b7f0da132b5757a3.

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,26 +1,11 @@
-import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
-import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { Injectable } from '@nestjs/common';
 import type { CreateUserDto } from './dto/create-user.dto';
 import type { UpdateUserDto } from './dto/update-user.dto';
-import { User } from './entities/user.entity';
 
 @Injectable()
 export class UserService {
-  private readonly _repo: Repository<User>;
-
-  constructor(@InjectRepository(User) repo: Repository<User>) {
-    this._repo = repo;
-  }
-
   create(createUserDto: CreateUserDto) {
     return `This action adds a new user: ${createUserDto.name}`;
-  }
-
-  async findAccount(account: string) {
-    const users = await this._repo.findOne({ where: { account: account } });
-
-    return users;
   }
 
   findAll() {
@@ -37,19 +22,5 @@ export class UserService {
 
   remove(id: number) {
     return `This action removes a #${id} user`;
-  }
-
-  async login(account: string, password: string) {
-    const user = await this.findAccount(account);
-
-    if (!user) {
-      throw new NotFoundException('wrong account');
-    }
-
-    if (user.password !== password) {
-      throw new BadRequestException('wrong password');
-    }
-
-    return user;
   }
 }
